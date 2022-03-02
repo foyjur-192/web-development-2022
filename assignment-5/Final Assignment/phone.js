747f38a40eaf4d8daaec306c9a2eb8349d973aea
foyjur-192 <97134368+foyjur-192@users.noreply.github.com>
2022-03-02 06:21:38 +0600

    add

--- assignment-5/Final Assignment/phone.js
+++ assignment-5/Final Assignment/phone.js
@@ -1,81 +1,69 @@
+//get input value
+//error handling for string value
+const main = document.getElementById('main');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 const searchButton = () => {
-    document.getElementById('mobile-container').innerHTML = ""; // Result will shown in first row
-    const searchInput = document.getElementById('search-input').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchInput}`;
-   fetch(url)
-   .then (response => response.json())
-   .then(data => mobileDetials(data.data));
+  const input = document.getElementById('input-value');
+  const error = document.getElementById('error');
+  const inputValue = input.value;
+  if(isNaN ('inputValue') == 'inputValue'){                 //=== data use later
+    error.innerText = "Brand not found";
+    input.value="";
+    main.innerHTML="";
+  }
+    else if (inputValue == 'brand'){
+      error.innerText="Please give a brand name";
+      input.value="";
+    }
 
-};
-
-const mobileDetials = (mobiles) => {
-    for (const data of mobiles){
-        const parent = document.getElementById('mobile-container');
-       const div = document.createElement("div");
-       div.classList.add("row")
-        div.classList.add("col-lg-4")
-        div.classList.add("mb-5")
-        div.innerHTML=`
-            <div class="card" style="width: 20rem;">
-                <img src="${data.image}" class="card-img-top mt-3" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${data.brand}</h5>
-                    <button onclick="cardDetails('${data.brand}')" class="btn btn-primary">See Details</button>
-                </div>
-            </div>
-        `
-        parent.appendChild(div);
+    else{
+      main.innerHTML="";  
+      const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+      fetch(url)
+      .then(res => res.json())
+      .then(data => cardDetails(data.data))
+      input.value="";
+      error.innerHTML="";
     }
 }
 
 
-const cardDetails = (brand) => {
- fetch(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`)
-  .then (response => response.json())
-  .then(data => {
-    const allBrand = data.memory;
-    const eachBrand = allBrand.find(brand => data.brand === brand)
-    console.log(eachBrand);
-  })
-
+const cardDetails = (cards) => {
+for(const card of cards){
+  const div = document.createElement('div');
+  div.classList.add('col-lg-4');
+  div.classList.add('mb-5');
+  div.innerHTML = `
+  <div class="card" style="width: 18rem;">
+  <img src="${card.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${card.phone_name}</h5>
+    <p class="card-text">${card.brand}</p>
+    <button href="#" onclick="featureDetails('${card.slug}')" class="btn btn-primary">Explore Features</button>
+  </div>
+</div>
+  
+  `
+  main.appendChild(div);
+}
 }
 
-*/
+const featureDetails = (slug) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${slug}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+  const allCards = data.cards;
+  console.log(allCards);
+}
+
+
+
+
+
+
+
+
+
+
+/**/
